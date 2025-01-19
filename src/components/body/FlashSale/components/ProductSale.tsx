21566ab5377d4d3f7cb0696858f92f2c0ecb0391
Tran Pham Thanh Nam <trannam.nt865@gmail.com>
2025-01-20 03:09:30 +0700

    Update ProductSale.tsx

--- src/components/body/FlashSale/components/ProductSale.tsx
+++ src/components/body/FlashSale/components/ProductSale.tsx
@@ -11,7 +11,7 @@ const ProductSale: React.FC<ProductCardProps> = ({ product }) => {
   const discountPercentage = Math.round(((originalPrice - salePrice) / originalPrice) * 100)
 
   return (
-    <div className='tw-bg-white tw-overflow-hidden tw-transition-transform hover:tw-transform hover:tw-scale-105 tw-h-[350px] sm:tw-h-[380px] tw-flex tw-flex-col tw-border-[2px] tw-cursor-pointer'>
+    <div className='tw-bg-white tw-overflow-hidden tw-transition-transform hover:tw-transform hover:tw-scale-105 tw-h-[380px]  tw-flex tw-flex-col tw-border-[2px] tw-cursor-pointer'>
       <div className='tw-relative tw-w-full tw-h-[250px]  tw-flex tw-justify-center tw-items-center'>
         <img src={image} alt={name} className='tw-object-cover tw-w-[95%] tw-h-[95%]' />
         <span className='tw-absolute tw-top-0 tw-left-0 tw-px-2 tw-py-1 tw-text-sm tw-text-white tw-bg-orange-500 tw-rounded-br-lg'>
@@ -20,9 +20,9 @@ const ProductSale: React.FC<ProductCardProps> = ({ product }) => {
       </div>
 
       <div className='tw-flex tw-flex-col tw-flex-grow tw-p-4'>
-        <h3 className='tw-text-lg tw-font-semibold tw-mb-2 tw-line-clamp-2 tw-h-[3.2rem]'>{name}</h3>
+        <h3 className='tw-text-lg tw-font-semibold tw-mb-1 tw-line-clamp-2 tw-h-14'>{name}</h3>
 
-        <div className='tw-flex tw-flex-col tw-mt-auto'>
+        <div className='tw-flex tw-flex-col '>
           <span className='tw-text-sm tw-text-gray-500 tw-line-through'>{originalPrice.toLocaleString()}đ</span>
           <span className='tw-text-xl tw-font-bold tw-text-red-500'>{salePrice.toLocaleString()}đ</span>
         </div>
